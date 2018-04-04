98fe2c239e3fbfd939fe386ef11a35dcb9eb5937
sirCrusader <sercrusader@gmail.com>
2018-04-04 14:45:53 +0300

    Added routes.

--- blockchain.py
+++ blockchain.py
@@ -5,6 +5,7 @@ from textwrap import dedent
 from time import time
 from uuid import uuid4
 
+import requests
 from flask import Flask, jsonify, request
 
 
@@ -82,7 +83,7 @@ class Blockchain(object):
 
         return hashlib.sha256(block_string).hexdigest()
 
-    def valid_proof(last_proof, proof):
+    def valid_proof(self, last_proof, proof):
         """
         Validates the Proof: Does hash(last_proof, proof) contain 4 leading zeros?
 
@@ -119,6 +120,25 @@ def mine():
 
     # We must receive a reward for finding the proof.
     # The sender is "0" to signify that this node has mined a new coin.
+    blockchain.new_transaction(
+        sender="0",
+        recipient=node_identifier,
+        amount=1
+    )
+
+    # Forge the new Block by adding it to the chain
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(proof, previous_hash)
+
+    response = {
+        'message': "New Block Forged",
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
+    return jsonify(response), 200
+
 
 @app.route('/transactions/new', methods=['POST'])
 def new_transaction():
